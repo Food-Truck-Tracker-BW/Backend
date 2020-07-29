b1391e57f683ed301fcabd30fb5c7d5d230fc8e4
PJ Wise <pjwise@PJs-MacBook-Pro.attlocal.net>
2020-07-28 22:41:54 -0400

    spatial

--- data/seeds/00_cleaner.js
+++ data/seeds/00_cleaner.js
@@ -2,6 +2,6 @@ const cleaner = require('knex-cleaner');
 
 exports.seed = function (knex) {
     return cleaner.clean(knex, {
-        ignoreTables: ['knex_migrations', 'knex_migrations_lock'], // don't empty migration tables
+        ignoreTables: ['spatial_ref_sys', 'knex_migrations', 'knex_migrations_lock'], // don't empty migration tables
     });
 };
